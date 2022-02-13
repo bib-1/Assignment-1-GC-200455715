0ec2a795515d73909256887d0c9a8d7ec7213241
Bibek Poudel <200455715@student.georgianc.on.ca>
2022-02-12 23:10:51 -0500

    Created insertIntoDB and exportCSVToDatabase to insert data into database and to export scv file to database.

--- src/main/java/com/example/assignment1gc200455715/DBUtility.java
+++ src/main/java/com/example/assignment1gc200455715/DBUtility.java
@@ -1,7 +1,11 @@
 package com.example.assignment1gc200455715;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Formatter;
 
 public class DBUtility {
 
@@ -57,11 +61,58 @@ public class DBUtility {
         return players;
     }
 
-    public static void main(String[] args) {
-        try {
-            System.out.println(getChessPlayersFromDB());
+    /*
+    * To load data from csv to database
+    * Reference: https://www.java67.com/2015/08/how-to-load-data-from-csv-file-in-java.html
+    **/
+    public static void exportCsvToDatabase(String filename) throws IOException, SQLException {
+
+        //using try with resource to read the file using BufferedReader
+        try(BufferedReader br = Files.newBufferedReader(Path.of(filename));
+        )   {
+            String line = br.readLine(); //to read the first line of the file
+
+            line = br.readLine(); // to skip the title line
+
+            while(line != null){
+
+                String [] playerArray = line.split(",");
+
+                //calling the insert function to inter the player array which contains the name, country, sex and rating
+                insertPlayerIntoDB(playerArray);
+
+                line = br.readLine();
+            }
+        }
+    }
+
+    public static void insertPlayerIntoDB( String [] array)throws SQLException{
+
+        //insert statement
+        String insertQuery = "INSERT INTO chess_player(name, country, sex, rating) VALUES(?, ?, ?, ?);";
+
+        try(
+                Connection conn = DriverManager.getConnection(DBCred.getConnectURL(), DBCred.getUserName(), DBCred.getPassword()); //connects to the database
+                PreparedStatement ps = conn.prepareStatement(insertQuery)
+        ){
+
+            //configuring the prepared statement to prevent SQL injection attacks
+            ps.setString(1, array[0]);
+            ps.setString(2, array[1]);
+            ps.setString(3, array[2]);
+            ps.setInt(4, Integer.parseInt(array[3]));
+
+            //run the command into DB
+            ps.executeUpdate();
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
+
+    public static void main(String[] args) throws IOException, SQLException {
+
+
+
+         }
 }
