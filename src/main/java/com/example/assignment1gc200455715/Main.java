d429592d3d743e1489511d86dc0e723892023903
Bibek Poudel <200455715@student.georgianc.on.ca>
2022-02-26 22:05:57 -0500

    Added img icon in title bar of a stage

--- src/main/java/com/example/assignment1gc200455715/Main.java
+++ src/main/java/com/example/assignment1gc200455715/Main.java
@@ -3,9 +3,12 @@ package com.example.assignment1gc200455715;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
 
 public class Main extends Application {
 
@@ -13,6 +16,9 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("players-gender-chart-view.fxml"));
+        // set icon
+        //How to add image reference: https://stackoverflow.com/questions/10275841/how-to-change-the-icon-on-the-title-bar-of-a-stage-in-java-fx-2-0-of-my-applicat
+        stage.getIcons().add(new Image(String.valueOf(Main.class.getResource("img/chess.png"))));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Chess Record");
         stage.setScene(scene);
